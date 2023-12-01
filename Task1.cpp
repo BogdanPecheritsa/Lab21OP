0ca619c0517f91afaa3eb6a73bbdd2db61e3fa31
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2023-12-01 13:15:56 +0200

    modified: Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -8,7 +8,7 @@ int main() {
     char str[100];
     int uppercaseCount = 0;
 
-    printf("¬вед≥ть р€док: ");
+    printf("¬вед≥ть р€дoк: ");
     scanf_s("%s", str, sizeof(str));
 
     for (int i = 0; str[i] != '\0'; ++i) {
